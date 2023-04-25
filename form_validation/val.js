2d077f6c7ea7397f4f01ae48afcb5df618c7a105
piyushghosh017 <piyushghosh2021@gmail.com>
2023-04-25 18:34:40 +0530

    rectification

--- form_validation/val.js
+++ form_validation/val.js
@@ -1,11 +1,27 @@
-function checkPass(txt){
-            
-    var pass=/^[A-Za-z0-9]\w{8,20}$/;
-    if(txt.value.match(pass)){
-        alert("Correct format");
+function checkPass(pass,user){
+    console.log(pass.value, user.value)
+        
+    // var pat=/^[A-Za-z0-9]\w{8,20}$/;
+    if(user.value.length === 0){
+        alert("Please Enter username");
+        return false;
+    }
+    else if(pass.value.length===0){
+        alert("Please Enter password");
+        return false;
+    }
+    else if(!pass.value.match( /[A-Za-z]{1,20}/ )){
+        alert("8 characters and at least 1 digit is required");
+        return false;
+    }
+    else if(!pass.value.match(/[0-9]{1,20}/)){
+        alert("8 characters and at least 1 digit is required");
+        return false;
+    }
+    else if( (pass.value.match( /[A-Za-z]{8,20}/ )) && (pass.value.match(/[0-9]{1,20}/)) ){
+        alert("Login Successfull");
         return true;
     }
-        alert("wrong format");
-        return false;
+        
     
 }
