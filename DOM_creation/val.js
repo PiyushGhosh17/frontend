d833af953e27f590aeb5af8b6d4e4db14303e191
piyushghosh017 <piyushghosh2021@gmail.com>
2023-04-27 10:23:18 +0530

    folder structure,forEach,eleminated using margin,styling in js,addEventListener,avoid else if

---
+++ DOM_creation/val.js
@@ -0,0 +1,38 @@
+const apps = [
+  { name: "Netflix", path: "images/netflix.png" },
+  { name: "Amazone Prime", path: "images/prime.png" },
+  { name: "Hotstar", path: "images/hotstar.jpeg" },
+];
+
+const elements = document.getElementById("apps");
+elements.style.fontSize = "40px";
+apps.forEach (item=>{
+    let image = document.createElement("img");
+    image.src = item.path;
+    image.width = 30;
+    image.height = 30;
+    image.style.borderRadius="5px";
+    image.style.marginRight="5px";
+    const txt = document.createTextNode(item.name);
+    const node = document.createElement("li");
+    node.append(image, txt);
+    elements.appendChild(node);
+}) 
+
+
+// function add(){
+//     var txt= document.getElementById('ele').value;
+//     const node=document.createElement("li");
+//     const textNode=document.createTextNode(txt);
+//     node.appendChild(textNode);
+//     document.getElementById("apps").appendChild(node);
+
+// }
+
+// function rev(){
+//     const element=document.getElementById("netflix");
+//     element.remove();
+// }
+
+
+
--- form_validation/val.js
+++ form_validation/val.js
@@ -1,27 +1,30 @@
-function checkPass(pass,user){
-    console.log(pass.value, user.value)
-        
-    // var pat=/^[A-Za-z0-9]\w{8,20}$/;
-    if(user.value.length === 0){
-        alert("Please Enter username");
-        return false;
-    }
-    else if(pass.value.length===0){
-        alert("Please Enter password");
-        return false;
-    }
-    else if(!pass.value.match( /[A-Za-z]{1,20}/ )){
-        alert("8 characters and at least 1 digit is required");
-        return false;
-    }
-    else if(!pass.value.match(/[0-9]{1,20}/)){
-        alert("8 characters and at least 1 digit is required");
-        return false;
-    }
-    else if( (pass.value.match( /[A-Za-z]{8,20}/ )) && (pass.value.match(/[0-9]{1,20}/)) ){
-        alert("Login Successfull");
-        return true;
-    }
-        
-    
-}
+let button = document.getElementById("form");
+let pass = document.getElementById("pass");
+let user = document.getElementById("user");
+
+const checkPass = (event) => {
+  event.preventDefault();
+  // var pat=/^[A-Za-z0-9]\w{8,20}$/;
+  if (user.value === "") {
+    alert("Please Enter username");
+    user.focus();
+    return false;
+  }
+  if (pass.value.length === 0 || pass.value.length < 8) {
+    alert("Password length should be 8");
+    pass.focus();
+    return false;
+  }
+  if (
+    !(pass.value.match(/[A-Za-z]{7,20}/) && pass.value.match(/[0-9]{1,20}/))
+  ) {
+    alert("8 characters and at least 1 digit is required");
+    pass.focus();
+    return false;
+  }
+
+  alert("Login Successfull");
+  return true;
+};
+
+button.addEventListener("click", checkPass);
