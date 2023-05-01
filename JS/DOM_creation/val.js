c948a7ed908ad1592989db667c54d012ab9d62bc
piyushghosh017 <piyushghosh2021@gmail.com>
2023-05-01 19:20:44 +0530

    removed redundant code

--- JS/DOM_creation/val.js
+++ JS/DOM_creation/val.js
@@ -6,21 +6,15 @@ const apps = [
 
 const elements = document.getElementById("apps");
 elements.style.fontSize = "40px";
-apps.forEach (item=>{
-    let image = document.createElement("img");
-    image.src = item.path;
-    image.width = 30;
-    image.height = 30;
-    image.style.borderRadius="5px";
-    image.style.marginRight="5px";
-    const txt = document.createTextNode(item.name);
-    const node = document.createElement("li");
-    node.append(image, txt);
-    elements.appendChild(node);
-}) 
-
-
-
-
-
-
+apps.forEach((item) => {
+  let image = document.createElement("img");
+  image.src = item.path;
+  image.width = 30;
+  image.height = 30;
+  image.style.borderRadius = "5px";
+  image.style.marginRight = "5px";
+  const txt = document.createTextNode(item.name);
+  const node = document.createElement("li");
+  node.append(image, txt);
+  elements.appendChild(node);
+});
